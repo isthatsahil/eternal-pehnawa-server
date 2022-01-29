2f49cffd3ce6a0510fa8edf08ca9ac7eacbc2ba7
isthatsahil <sahil.verma997@gmail.com>
2022-01-30 00:44:40 +0530

    First commit

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.json({
+    message: "Welcome to the API",
+  });
+});
 app.post("/api/razorpay/create-order", async (req, res) => {});
 
 app.listen(process.env.PORT || 5000, () =>
